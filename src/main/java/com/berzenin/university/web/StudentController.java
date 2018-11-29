c5481bd411f51b63e41db4fbc6bb42533cb5e7a8
Filipp Berzenin <filipp.berzenin@gmail.com>
2018-11-29 20:24:19 +0200

    web-service 20:21 29/11/18

--- src/main/java/com/berzenin/university/web/StudentController.java
+++ src/main/java/com/berzenin/university/web/StudentController.java
@@ -1,33 +1,72 @@
 package com.berzenin.university.web;
 
-import com.berzenin.university.dao.StudentRepository;
-import com.berzenin.university.model.persons.Student;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.berzenin.university.dao.StudentRepository;
+import com.berzenin.university.model.persons.Student;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class StudentController {
 
-    private final StudentRepository studentRepository;
+	
+	@Autowired
+	private final StudentRepository studentRepository;
 
-    @GetMapping(
-            value = "/students",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    List<Student> getAll(){
-        return studentRepository.findAll();
-    }
+	@GetMapping(
+			value = "/students", 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	List<Student> getAll() {
+		return studentRepository.findAll();
+	}
+	
+	@GetMapping(
+			value = "/students/{id}", 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	Student getStudentsById(@PathVariable("id") long id) {
+		return studentRepository.getOne(id);
+	}
 
-    @PostMapping(
-            value = "/students",
-            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
-    Student addStudent(@RequestBody Student student){
-        return studentRepository.save(student);
-    }
+	@PostMapping(
+			value = "/students", 
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	Student addStudent(@RequestBody Student student) {
+		return studentRepository.save(student);
+	}
+	
+	@PutMapping(
+			value = "/students/update/{id}",
+			consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, 
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	Student updateStudent(@RequestBody Student student, @PathVariable("id") long id) {
+		Student studentForUpdate = studentRepository.findById(id).get();
+		studentForUpdate.setName(student.getName());
+		studentForUpdate.setSurename(student.getSurename());
+		return studentRepository.save(studentForUpdate);
+	}
+	
+	@DeleteMapping(value = "/students/delete/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	Student deleteStudentByEntity (@PathVariable("id") long id) {
+		Student student = studentRepository.getOne(id);
+		studentRepository.delete(student);
+		return student;
+	}
 }
--- src/test/java/com/berzenin/university/web/StudentRestEndpointIntegrationTest.java
+++ src/test/java/com/berzenin/university/web/StudentRestEndpointIntegrationTest.java
@@ -3,6 +3,7 @@ package com.berzenin.university.web;
 import com.berzenin.university.dao.StudentRepository;
 import com.berzenin.university.model.persons.Student;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,16 +16,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Optional;
 
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,54 +35,99 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 public class StudentRestEndpointIntegrationTest {
 
-    @Autowired
-    MockMvc subject;
+	@Autowired
+	MockMvc subject;
 
-    @Autowired
-    ObjectMapper mapper;
+	@Autowired
+	ObjectMapper mapper;
 
-    @MockBean
-    StudentRepository studentRepository;
+	@MockBean
+	StudentRepository studentRepository;
 
-    @Before
-    public void setUp() {
-        assertThat(subject).isNotNull();
-    }
+	@Before
+	public void setUp() {
+		assertThat(subject).isNotNull();
+	}
 
-    @Test
-    public void testGetAllStudents() throws Exception {
-        when(studentRepository.findAll()).thenReturn(Arrays.asList(
-                new Student(1, "Alex", "Ro"),
-                new Student(2, "Mary", "Bo")));
+	@Test
+	public void testGetAllStudents() throws Exception {
+		when(studentRepository.findAll())
+				.thenReturn(Arrays.asList(
+						new Student(1, "Alex", "Ro"), 
+						new Student(2, "Mary", "Bo")));
 
-        subject.perform(get("/students"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id").value(1))
-                .andExpect(jsonPath("$[0].name").value("Alex"))
-                .andExpect(jsonPath("$[0].surename").value("Ro"))
-                .andExpect(jsonPath("$[1].id").value(2))
-                .andExpect(jsonPath("$[1].name").value("Mary"))
-                .andExpect(jsonPath("$[1].surename").value("Bo"));
-    }
+		subject.perform(get("/students"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+			.andExpect(jsonPath("$").isArray())
+			.andExpect(jsonPath("$", hasSize(2)))
+			.andExpect(jsonPath("$[0].id").value(1))
+			.andExpect(jsonPath("$[0].name").value("Alex"))
+			.andExpect(jsonPath("$[0].surename").value("Ro"))
+			.andExpect(jsonPath("$[1].id").value(2))
+			.andExpect(jsonPath("$[1].name").value("Mary"))
+			.andExpect(jsonPath("$[1].surename").value("Bo"));
+	}
 
-    @Test
-    public void testAddStudent() throws Exception {
-        Student student = new Student("Some", "Name");
-        when(studentRepository.save(any())).thenReturn(new Student(1, "Some", "Name"));
+	@Test
+	public void testAddStudent() throws Exception {
+		Student student = new Student("Some", "Name");
+		when(studentRepository.save(any())).thenReturn(new Student(1, "Some", "Name"));
 
-        subject.perform(post("/students")
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content(mapper.writeValueAsBytes(student)))
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("Some"))
-                .andExpect(jsonPath("$.surename").value("Name"));
+		subject.perform(post("/students")
+			.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+			.content(mapper.writeValueAsBytes(student)))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.id").value(1))
+			.andExpect(jsonPath("$.name").value("Some"))
+			.andExpect(jsonPath("$.surename").value("Name"));
 
-        verify(studentRepository).save(new Student("Some", "Name"));
-    }
+		verify(studentRepository).save(new Student("Some", "Name"));
+	}
+	
+	@Test
+	public void testFindStudentById() throws Exception {
+		Long id = new Long(1);
+		when(studentRepository.findById(id)).thenReturn(Optional.ofNullable(new Student(1, "Fil", "Berzenin")));		
+		subject.perform(get("/students/"+id)
+			.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+			.andDo(print())
+			.andExpect(status().isOk());
+
+			verify(studentRepository).getOne(id);
+	}
+
+	@Test
+	public void testUpdateStudent() throws Exception {
+		Long id = new Long(1);
+		Student studentForUpdate = new Student(1, "Tima", "Berzenin");
+		Student studentWithOldParam = new Student("Tima", "Berzen");
+		when(studentRepository.findById(id)).thenReturn(Optional.ofNullable(studentWithOldParam));
+		when(studentRepository.save(any())).thenReturn(studentForUpdate);
+
+		subject.perform(put("/students/update/"+id)
+			.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+			.content(mapper.writeValueAsBytes(studentForUpdate)))
+			.andDo(print())
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.id").value(id))
+			.andExpect(jsonPath("$.name").value("Tima"))
+			.andExpect(jsonPath("$.surename").value("Berzenin"))
+			.andReturn();
+	}
+	
+	@Test
+	public void testDeleteStudentsById() throws Exception {
+		Long id = new Long(1);
+		Student studentForDelete = new Student(1, "Tima", "Berzenin");
+		when(studentRepository.findById(id)).thenReturn(Optional.ofNullable(studentForDelete));
+		subject.perform(delete("/students/delete/"+id)
+				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andDo(print())
+				.andExpect(status().isNoContent());
+		
+		verify(studentRepository).getOne(id);
+	}
 }

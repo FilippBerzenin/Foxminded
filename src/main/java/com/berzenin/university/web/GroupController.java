84dbaa7506fc7c4085a25b369a8d116a257f8dcf
Konstantin Kosmachevskiy <k.kosmachevskiy@gmail.com>
2018-12-03 22:17:56 +0200

    Fixes

--- src/main/java/com/berzenin/university/web/GroupController.java
+++ src/main/java/com/berzenin/university/web/GroupController.java
@@ -1,10 +1,12 @@
 package com.berzenin.university.web;
 
 import com.berzenin.university.dao.GroupRepository;
+import com.berzenin.university.model.persons.Student;
 import com.berzenin.university.model.university.Group;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +24,10 @@ public class GroupController {
 		return groupRepository.findAll();
 	}
 
+	@GetMapping(
+			value = "/groups/{id}/students",
+			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<Student> students(@PathVariable("id") Long groupId){
+    	return null;
+	}
 }

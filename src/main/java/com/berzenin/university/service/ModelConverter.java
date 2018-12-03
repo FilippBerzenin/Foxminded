7f2dbe53013f134b3a1287d8c2f63d5c686df8cc
Filipp Berzenin <filipp.berzenin@gmail.com>
2018-12-03 15:40:39 +0200

    task web-service 15:40 03/12/18

--- src/main/java/com/berzenin/university/service/ModelConverter.java
+++
@@ -1,6 +0,0 @@
-package com.berzenin.university.service;
-
-public interface ModelConverter<M, V> {
-
-    V toView(M model);
-}
---
+++ src/main/java/com/berzenin/university/web/exception/ExceptionsHandler.java
@@ -0,0 +1,36 @@
+package com.berzenin.university.web.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionsHandler extends ResponseEntityExceptionHandler {
+
+	@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Student Not Found")
+	@ExceptionHandler(StudentNotFoundException.class)
+	@ResponseBody
+	protected void handleThereIsStudentNotFoundException() {
+		log.error("Student Not Found");;
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+		logger.error("Request: " + req.getRequestURL() + " raised " + ex);
+
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("exception", ex);
+		mav.addObject("url", req.getRequestURL());
+		mav.setViewName("error");
+		return mav;
+	}
+}

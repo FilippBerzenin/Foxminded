8fac989d621457b878bc3df18fefb8691d93d8cf
Filipp Berzenin <filipp.berzenin@gmail.com>
2019-01-06 17:44:33 +0200

    after little change

--- src/main/java/com/berzenin/university/model/persons/Teacher.java
+++ src/main/java/com/berzenin/university/model/persons/Teacher.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import com.berzenin.university.model.university.Course;
 
-import lombok.Data;
-
-@Data
 public class Teacher extends Person {
 	
 	private List<Course> courses;

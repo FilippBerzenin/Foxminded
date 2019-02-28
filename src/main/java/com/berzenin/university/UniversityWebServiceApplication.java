1e98bc4f896a64d37dbb3da7b890dc9bf9e6068b
Filipp Berzenin <filipp.berzenin@gmail.com>
2019-02-28 11:15:20 +0200

    disable package "restcontroller"

--- src/main/java/com/berzenin/university/UniversityWebServiceApplication.java
+++ src/main/java/com/berzenin/university/UniversityWebServiceApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.berzenin.university" },
+excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.berzenin.university.web.restcontroller"))
 public class UniversityWebServiceApplication extends SpringBootServletInitializer  {
 
 	@Override
@@ -16,9 +20,4 @@ public class UniversityWebServiceApplication extends SpringBootServletInitialize
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityWebServiceApplication.class, args);
 	}
-//	
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return builder.sources(UniversityWebServiceApplication.class);
-//	}
 }
